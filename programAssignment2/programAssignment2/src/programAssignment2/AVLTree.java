1ef229cdf41cd322166c7b8c2d1134d206ff6ee7
Amy Tran <149899514+Froggie-A@users.noreply.github.com>
2024-10-24 22:06:43 -0500

    Update AVLTree.java

--- programAssignment2/programAssignment2/src/programAssignment2/AVLTree.java
+++ programAssignment2/programAssignment2/src/programAssignment2/AVLTree.java
@@ -1,260 +1,218 @@
-package programAssignment2;
-import java.util.Scanner;
-//Node structure
-class Node {
-	int orderID;
-	String bookName;
-	int height;
-	Node left;
-	Node right;
+
+public class AVLTree {
 	
-	//Constructor for creating a new node
-	Node(int orderID, String bookName){
-		this.orderID = orderID;
-		this.bookName = bookName;
-		this.height = 1;
+	public static class Node {
+		int orderID;
+		String name;
+		Node left;
+		Node right;
+		int height;
+		
+		public Node(int orderID,String name) {
+			this.height = 1;
+			this.name = name;
+			this.left = null;
+			this.right = null;
+			this.orderID = orderID;
+		}
 	}
-}
-class AVLTree {
-	Node root;
+	Node Root;
 	
-	//get height of the node
-	int nodeHeight(Node n) {
-		if(n == null)
-			return 0;
-		return n.height;
-	}
-	
-	//get current node count
-	int countNodes(Node n) {
-	    if (n == null)
-	        return 0;
-	    return 1 + countNodes(n.left) + countNodes(n.right);
-	}
-	
-	//update height of the node
-	void updateHeight(Node n) {
-		n.height = 1+ Math.max(nodeHeight(n.left), nodeHeight(n.right));
-	}
-	
-	//balance of the node
-	int balance(Node n) {
-		if(n == null) {
+	private int height(Node node) {
+		if (node == null) {
 			return 0;
 		}
-		return (nodeHeight(n.left) - nodeHeight(n.right));
-	}
-	
-	//left rotate
-	Node leftRotate(Node x) {
-		Node y = x.right;
-		Node T2 = y.left;
-		
-		//perform rotation
-		y.left = x;
-		x.right = T2;
-		
-		//update height
-		updateHeight(x);
-		updateHeight(y);
-		
-		return y; //new root
-	}
-	
-	//right rotation
-	Node rightRotate(Node x) {
-		Node y = x.left;
-		Node T2 = y.right;
-		
-		//perform rotation
-		y.right = x;
-		x.left = T2;
-		
-		//update height
-		updateHeight(y);
-		updateHeight(x);
-		
-		return x; //new root
-	}
-	
-	//balance the tree
-	Node balanceTree(Node n) {
-		updateHeight(n);
-		
-		int balance = balance(n);
-		
-		if(balance > 1) {
-			//right
-			if(balance(n.right) < 0) {
-				//right left
-				n.right = rightRotate(n.right);
-				return leftRotate(n);
-			}
-		else //RR
-			return leftRotate(n);	
+		else{
+			return node.height;	
 		}
-		if(balance < -1) {
-			//left
-			if(balance(n.left) > 0) {
-				//left right
-				n.left = leftRotate(n.left);
-				return rightRotate(n);
-			}
-		else //LL
-			return rightRotate(n);	
-		}
-		return n;
 	}
+
 	
-	//Insert
-	Node insertNode(Node n, int key, String bookName) {
-		//perform standard insert
-		if(n == null) {
-			return new Node(key, bookName);
-		}
-		else if(key < n.orderID) {
-			n.left = insertNode(n.left, key, bookName);
+	public int updateHeight(Node node) {
+		if (node == null) {
+			return -1;	
 		}
 		else {
-			n.right = insertNode(n.right, key, bookName);
+			node.height = 1+ Math.max(updateHeight(node.left),updateHeight(node.right));
 		}
-		return balanceTree(n);// Duplicate orderID not allowed
+		return node.height;
 	}
 	
-	//return the next left child
-	Node mostLeftChild(Node n) {
-		if(n.left != null)
-			return mostLeftChild(n.left);
-		else
-			return n;
+	private Node rightRotation(Node node) {
+		Node y = node.left;
+		Node temp = y.right;
+		y.right = node;
+		node.left = temp;
+		updateHeight(node);
+		updateHeight(y);
+		
+		return y;
 	}
 	
-	//remove
-	Node removeNode(Node n, int key) {
-	    //standard remove
-		if (n == null) {
-	        return n;
-	    } 
-		// If the key to be deleted is smaller than the root's key, it lies in left subtree
-		else if (n.orderID > key) {
-	        n.left = removeNode(n.left, key);
-	    } 
-		// If the key to be deleted is greater than the root's key, it lies in right subtree
-		else if (n.orderID < key) {
-	        n.right = removeNode(n.right, key);
-	    } 
-		// If the key is same as root's key, this is the node to be deleted
+	private Node leftRotation(Node node) {
+		Node y = node.right;
+		Node temp = y.left;
+		y.left = node;
+		node.right = temp;
+		updateHeight(node);
+		updateHeight(y);
+		
+		return y;
+	}
+	
+	private Node balanceTree(Node root) {
+		int balancingFactor = calcBF(root);
+		
+		if (balancingFactor < -1) {
+			if (calcBF(root.right) > 0) {
+				root.right = rightRotation(root.right);
+			}
+			return leftRotation(root);
+		}
+		
+		if (balancingFactor > 1) {
+			if (calcBF(root.left) < 0) {
+				root.left = leftRotation(root.left);
+			}
+			return rightRotation(root);
+		}
+		return root;
+	}
+	
+	private int calcBF(Node node) {
+		if(node == null) {
+			return 0;
+		}
 		else {
-			// Node with only one child or no child
-	        if (n.left == null || n.right == null) {
-	            n = (n.left == null) ? n.right : n.left;
-	        } else {
-	            Node mostLeftChild = mostLeftChild(n.right);
-	            n.orderID = mostLeftChild.orderID;
-	            n.bookName = mostLeftChild.bookName;
-	            n.right = removeNode(n.right, n.orderID);
-	        }
-	    }
-	    if (n != null) {
-	        n = balanceTree(n);
-	    }
-	    return n;
+			return height(node.left) - height(node.right);
+		}
 	}
 	
-	//find book based on its order ID
-	Node findNode(int key) {
-	    Node current = root;
-	    while (current != null) {
-	        if (current.orderID == key) {
-	            break;
+	private  Node insert(Node root, int orderID, String name) {
+		 if (root == null) {
+	         return new Node(orderID,name) ;
+		 }    
+	       else if(orderID < root.orderID) {
+			root.left = insert(root.left,orderID,name);
+	     }
+	       else if(orderID > root.orderID){
+			root.right = insert(root.right,orderID,name);
+		}else {
+			System.out.println("OrderID alredy exists.");
+			return root;
+		}
+		updateHeight(root);
+		return balanceTree(root);
+	}
+	
+	public void insert(int orderID, String name) {
+		Root = insert(Root, orderID, name);
+	}
+	
+	 public void InOrder(Node root){
+	        if(root == null){
+	            return;
 	        }
-	        current = current.orderID < key ? current.right : current.left;
-	    }
-	    return current;
-	}    
+	        InOrder(root.left);
+	        System.out.println("(OrderID: " + root.orderID+") "+ "Book Name: "+ root.name );
+	        InOrder(root.right);
+	 }
 	
-	// Utility function for search of node
-	int search(int key)
-    {
-        if(findNode(key) == null)
-            return 0;
-        else
-            return 1;
-    }
+	public void InOrder() {
+		InOrder(Root);
+	} 
 	
-	 // Utility function for insertion of node
-    void add(int key, String bookName)
-    {
-        if (findNode(key) == null)
-        {
-            root = insertNode(root , key, bookName);
-            System.out.println("Insertion successful");
-        }
+	public Node search(Node node, int orderID) {
+		if(node == null || node.orderID == orderID) {
+			return node;
+		}
+		
+		if(orderID < node.orderID) {
+			return search(node.left,orderID);
+		}
+		return search(node.right,orderID);
+	}
+	
+	public Node findMin(Node node) {
+		Node current = node;
+		while(current.left != null) {
+			current = current.left;
+		}
+		return current;
+	}
+	
+	public Node findMax(Node node) {
+		Node current = node;
+		while(current.right != null) {
+			current = current.right;
+		}
+		return current;
+	}
+	
+	private Node remove(Node node, int orderID) {
+		if(node == null) {
+			System.out.println("The Order doesn't exist.");
+			return node;
+		}
+		
+		if (orderID < node.orderID) {
+			node.left = remove(node.left,orderID);
+		}else if(orderID > node.orderID) {
+			node.right = remove(node.right,orderID);
+		}else {
+			if(node.left == null || node.right == null) {
+				
+				Node temp;
+				
+				if(node.left != null) {
+					temp = node.left;
+				}else {
+					temp = node.right;
+				}
+				
+				if(temp == null) {
+					node = null;
+					
+				}else {
+					node = temp;
+				}
+				
+		}else {
+			Node temp = findMin(node.right);
+			
+			node.orderID = temp.orderID;
+			node.name = temp.name;
+			node.right = remove(node.right,temp.orderID);
+		}
+	}
+	
+	if(node == null) {
+		return node;
+	}
+	
+	updateHeight(node);
+	return balanceTree(node);
+	
+	}
+	
+	public void remove(int orderID) {
+		Root = remove(Root,orderID);
+	}
+	
+	public int getHeightTree() {
+		return height(Root);
+	}
 
-        else
-            System.out.println("\nKey with the entered value already exists in the tree");
-    }
+	 public int getNodeCount(Node node) {
+	        if (node == null) {
+	        	return 0;
+	        }
+	        return 1 + getNodeCount(node.left) + getNodeCount(node.right);
+	 }
+	 
+	 public int getNodeCount() {
+		 return getNodeCount(Root);
+	 }
+	 	 }
+	
 
-    // Utility function for deletion of node
-    void delete(int key)
-    {
-        if (findNode(key) != null)
-        {
-            root = removeNode(root , key);
-            System.out.println("\nDeletion successful ");
-        }
-
-        else
-            System.out.println("\nNo node with entered value found in tree");
-    }
-
-    //An in-order traversal of the AVL tree prints the orders in ascending order by their order IDs.
-    void InOrder(Node root)
-    {
-        if(root == null)
-        {
-            System.out.println("\nNo nodes in the tree");
-            return;
-        }
-
-        if(root.left != null)
-            InOrder(root.left);
-        System.out.print(root.orderID + " ");
-        if(root.right != null)
-            InOrder(root.right);
-
-    }
-
-    void PreOrder(Node root)
-    {
-        if(root == null)
-        {
-            System.out.println("No nodes in the tree");
-            return;
-        }
-
-        System.out.print(root.bookName + " ");
-        if(root.left != null)
-            PreOrder(root.left);
-        if(root.right != null)
-            PreOrder(root.right);
-
-    }
-
-    void PostOrder(Node key)
-    {
-        if(key == null)
-        {
-            System.out.println("No nodes in the tree");
-            return;
-        }
-
-
-        if(key.left != null)
-            PostOrder(key.left);
-        if(key.right != null)
-            PostOrder(key.right);
-        System.out.print(key.bookName + " ");
-
-    }
-}
